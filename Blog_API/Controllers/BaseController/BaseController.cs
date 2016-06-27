7b0303af8814b9474f949942d0c0a1a8eddd360c
A-Frantyk <ifrantya@gmail.com>
2016-06-27 17:09:44 +0300

    References to the API project added

---
+++ Blog_API/Controllers/BaseController/BaseController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Blog_API.Controllers.BaseController
+{
+    public class BaseController : ApiController
+    {
+
+    }
+}
